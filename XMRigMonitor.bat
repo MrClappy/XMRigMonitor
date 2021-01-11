c5ab2e34d6df97c7b384d9d76791e897f9e036a9
MrClappy <ryan.macneille@gmail.com>
2021-01-11 02:21:45 -0500

    Added CPU temperature monitor support

--- XMRigMonitor.bat
+++ XMRigMonitor.bat
@@ -15,17 +15,27 @@ FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto 
 goto PULSE
 
 :FOUND
+start C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\OpenHardwareMonitorReport.exe ReportToFile -f C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pull.txt --IgnoreMonitorGPU --IgnoreMonitorHDD --IgnoreMonitorRAM --IgnoreMonitorFanController
+for /f "tokens=2 delims=:" %%a in ('type C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pull.txt^|find "/amdcpu/0/temperature/0"') do (
+  echo %%a > C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pulled.txt
+)
+for /f "tokens=3" %%a in (C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pulled.txt) do set TEMP=%%a
+echo [%date% %time%] Last CPU Temp: %TEMP%C > C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\lasttemp.txt
+del C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pull.txt
+del C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\pulled.txt
 cls && echo [%date% %time%] Still running...
 timeout /t %PULSETIME% > nul
 goto PULSE
 
 :XMRIG_CRASH
 echo [%date% %time%] XMRig Crash Recovered, script monitoring... >> C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\logs\Script_log.txt
+type C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\lasttemp.txt >> C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\logs\Script_log.txt
 CALL C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\Crash.bat 1
 goto PULSE
 
 :SYSTEM_CRASH
 echo [%date% %time%] Script Triggered By System, Checking Network... >> C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\logs\Script_log.txt
+type C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\OpenHardwareMonitorReport\lasttemp.txt >> C:\Users\Ryan\Desktop\xmrig-6.7.0\backend\logs\Script_log.txt
 goto RECOVERY
 
 :RECOVERY
