c3ac5bdb25bba20987c437ec4ad30bc576e19993
MrClappy <ryan.macneille@gmail.com>
2021-02-11 04:12:52 -0500

    Removed unneeded duplicates

--- backend/EmailConfig.bat
+++ backend/EmailConfig.bat
@@ -8,12 +8,4 @@ SET Recipient="%~6"
 SET SMTPServer="%~7"
 SET SMTPPortNumber="%~8"
 
-REM Uncomment to send email when XMRig app crashes
-if "%CrashType%" == "1" (
-    Powershell -ExecutionPolicy Bypass -Command "& '%~dp0\Emailer.ps1' '%EmailAccount%' '%EmailPassword%' '%Subject%' %LogFile% %Recipient% %SMTPServer% %SMTPPortNumber%"
-) 
-
-if "%CrashType%" == "2" (
-    Powershell -ExecutionPolicy Bypass -Command "& '%~dp0\Emailer.ps1' '%EmailAccount%' '%EmailPassword%' '%Subject%' %LogFile% %Recipient% %SMTPServer% %SMTPPortNumber%"
-)
-
+Powershell -ExecutionPolicy Bypass -Command "& '%~dp0\Emailer.ps1' '%EmailAccount%' '%EmailPassword%' '%Subject%' %LogFile% %Recipient% %SMTPServer% %SMTPPortNumber%"
